bf9f54b670ff81c0a86e71b30d8d3a2cb9c22f11
Jose Alejandro Barrientos Suarez <alejandrobarrientossuarez@gmail.com>
2026-05-24 19:27:17 -0600

    Convertir tabs a espacios

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ class KuramotoDiscreto:
             }
             if t < T:
                 fila["S_i(theta_t)"] = tuple(int(x) for x in S_hist[t])
-		        fila["sigma_kappa(S_i)"] = tuple(int(x) for x in sigma_hist[t])
+                fila["sigma_kappa(S_i)"] = tuple(int(x) for x in sigma_hist[t])
             else:
                 fila["S_i(theta_t)"]     = "-"
                 fila["sigma_kappa(S_i)"] = "-"
@@ -672,3 +672,4 @@ with tab4:
     for img in imagenes:
         st.image(img["ruta"], caption=img["titulo"], use_column_width=True)
         st.divider()
+
