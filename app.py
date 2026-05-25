1ef5c42ffad650f4e2db513f35d8fcc6f56be38b
Jose Alejandro Barrientos Suarez <alejandrobarrientossuarez@gmail.com>
2026-05-24 19:15:00 -0600

    Corregir formato numpy en tabla trayectoria

--- app.py
+++ app.py
@@ -174,13 +174,13 @@ class KuramotoDiscreto:
         for t in range(T + 1):
             fila = {
                 "t":              t,
-                "theta(t)":       str(tuple(trayectoria[t])),
+                "theta(t)": tuple(int(x) for x in trayectoria[t]),
                 "r(t)":           round(r_hist[t], 4),
-                "subred_acuerdo": str(subredes[t]),
+                "subred_acuerdo": tuple((int(a), int(b)) for a, b in resultado["subredes"][t]),
             }
             if t < T:
-                fila["S_i(theta_t)"]     = str(tuple(S_hist[t]))
-                fila["sigma_kappa(S_i)"] = str(tuple(sigma_hist[t]))
+                fila["S_i(theta_t)"] = tuple(int(x) for x in S_hist[t])
+		fila["sigma_kappa(S_i)"] = tuple(int(x) for x in sigma_hist[t])
             else:
                 fila["S_i(theta_t)"]     = "-"
                 fila["sigma_kappa(S_i)"] = "-"
